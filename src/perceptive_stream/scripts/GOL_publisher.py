612fb9b53a55b4bd797dd921f64d6dce96fbc2a5
BuildTools <unconfigured@null.spigotmc.org>
2021-06-09 14:29:22 +0200

    new branch

--- src/perceptive_stream/scripts/GOL_publisher.py
+++ src/perceptive_stream/scripts/GOL_publisher.py
@@ -10,6 +10,84 @@
 
 import rospy
 import numpy as np
-import utilsant as toto
+import cv2 as cv
+from threading import Lock
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+from pyquaternion import Quaternion
+from perceptive_stream.msg import Img, BBox3D
 
-print(toto.getTCCw())
+from bbox_manager import BBoxManager
+from queue_manager import QueueROSmsgs
+
+class BBoxProj:
+    #pub : the publisher
+    #pub_mutex : the mutex to avoid publishing in the same time. 
+
+    def __init__(self):
+        # init ROS 
+        rospy.init_node("bbox_reproj", anonymous=True)
+        queue_size = rospy.get_param('~queue_size')
+
+        # create a publisher
+        self.pub = rospy.Publisher('projector/img', Image, queue_size=10)
+        self.pub_mutex = Lock()
+
+        # Creat an OpenCV bridge
+        self.cv_bridge = CvBridge()
+
+        # Create a queue for the bounding box. Used to find a matching Bbox with an image
+        self.Bbox_queue = QueueROSmsgs(queue_size, debug=True)
+
+        self.bboxMgr= BBoxManager()
+
+        rospy.Subscriber('camera/image', Img, self.callback_img)
+        rospy.Subscriber('car/info', BBox3D, self.callback_bbox)
+
+        rospy.spin()
+    
+    def callback_img(self, data):
+        # Display and record the frame ID number (used to synchronize)
+        img_id = data.header.frame_id.split('.')[1]
+        rospy.loginfo("Received img : %s", img_id)
+
+        # There can be several bounding box (one per vehicles)
+        listOfBBox = []
+        bboxFound = True
+        while(bboxFound):
+            bboxIdx = self.Bbox_queue.searchFirst(".%s"%img_id) # Looking for corresponding bounding box with this image
+            if bboxIdx == -1:
+                bboxFound = False 
+                break # there is no bounding box anymore, let's get out of the loop
+            listOfBBox.append(self.Bbox_queue.pop(bboxIdx)) # remove the corresponding bounding box from the que and add it to the local list
+
+        img_msg = Image()
+
+        if len(listOfBBox) > 0:
+            # If there are some bounding box, draw them on the image
+            # img_msg = self.drawBoxes(data, listOfBBox)
+            (img_msg, bbox2D) = self.bboxMgr.draw2DBoxes(data, listOfBBox)
+            for bbox in listOfBBox:
+                rospy.loginfo("position : X %3.2fm \tY %3.2fm"%(bbox.vehicle.position.x, bbox.vehicle.position.y))
+            for bbox in bbox2D:
+                rospy.loginfo("BBox : ")
+                for bbox_pt in bbox:
+                    rospy.loginfo("    x: %d  y: %d"%(bbox_pt[0], bbox_pt[1]))
+        else:
+            # if there is no bounding box, just pipe the image
+            img_msg = data.image
+
+        img_msg.header = data.header
+
+        self.pub_mutex.acquire()        # Is it mendatory?
+        try:
+            self.pub.publish(img_msg) # publish
+        finally:
+            self.pub_mutex.release()
+
+    def callback_bbox(self, data):
+        self.Bbox_queue.add(data) # just add the read bounding box to the queue
+
+
+if __name__ == '__main__':
+    proj_node = BBoxProj()
---
+++ src/perceptive_stream/scripts/bbox_manager.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import rospy
+import numpy as np
+import cv2 as cv
+from threading import Lock
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+from pyquaternion import Quaternion
+from perceptive_stream.msg import Img, BBox3D
+
+from utilsant import getTCCw, pose2Tmat
+
+class BBoxManager:
+    # cv_bridge : bridge openCV <-> ROS
+
+    def __init__(self):
+
+        # Creat an OpenCV bridge
+        self.cv_bridge = CvBridge()
+
+
+
+    def drawBoxes(self, image, bboxes):
+        # take as input the image and the list of bounding box to draw
+        # apply the draw to the image
+
+        # convert the Image message to opoenCV format
+        cv_img = self.cv_bridge.imgmsg_to_cv2(image.image, "bgr8")
+
+        # information about the image an drawing parameters
+        h, w, depth = cv_img.shape
+        color = (0, 255, 0)
+        thickness = 2
+
+
+        for bbox in bboxes:
+            #for each bounding box
+
+            bbox_vert_world = self.placeBBoxInWorld(bbox) # place the 8 corner of the box in the world
+            bbox_vert_cam = self.compute1bbox(image, bbox_vert_world)   # project the points in the camera plane
+
+            points = [(int(x[0]), int(x[1])) for x in bbox_vert_cam] # we need tuple for the points
+
+            rospy.loginfo("vertex :\n%s"%points)  
+
+            # There can be no points if the bounding box is behind the camera. 
+            if len(points) > 0:
+                # draw each edges
+                for idx in range(len(points)-1):
+                    cv_img = cv.line(cv_img, points[idx], points[idx+1], color, thickness)
+                cv_img = cv.line(cv_img, points[0], points[5], color, thickness)
+                cv_img = cv.line(cv_img, points[1], points[6], color, thickness)
+                cv_img = cv.line(cv_img, points[2], points[7], color, thickness)
+                cv_img = cv.line(cv_img, points[4], points[7], color, thickness)
+                cv_img = cv.line(cv_img, points[0], points[3], color, thickness)
+
+        return self.cv_bridge.cv2_to_imgmsg(cv_img, "bgr8") # convert the openCV image to a ROS Image message
+
+    def draw2DBoxes(self, image, bboxes):
+        # take as input the image and the list of bounding box to draw
+        # apply the draw to the image
+
+        # convert the Image message to opoenCV format
+        cv_img = self.cv_bridge.imgmsg_to_cv2(image.image, "bgr8")
+
+        # information about the image an drawing parameters
+        h, w, depth = cv_img.shape
+        color = (0, 255, 0)
+        thickness = 2
+
+        setOfBbox = []
+
+        for bbox in bboxes:
+            #for each bounding box
+
+            bbox_vert_world = self.placeBBoxInWorld(bbox) # place the 8 corner of the box in the world
+            bbox_vert2D_cam = self.compute2Dbbox(image, bbox_vert_world)
+
+            if len(bbox_vert2D_cam) > 0:
+                setOfBbox.append(bbox_vert2D_cam)
+
+            for i in range(len(bbox_vert2D_cam)):
+                cv_img = cv.line(cv_img, bbox_vert2D_cam[i], bbox_vert2D_cam[(i+1)%len(bbox_vert2D_cam)], color, thickness)
+
+        return (self.cv_bridge.cv2_to_imgmsg(cv_img, "bgr8"), setOfBbox) # convert the openCV image to a ROS Image message
+
+    
+    def compute1bbox(self, image, bbox):
+        # get intrinsec parameters (K) matrix of the camera.
+        K = np.array(image.info.K)
+        K = np.reshape(K, (3, 3))
+
+        # Get the position of the camera
+        T_WCw = pose2Tmat(image.pose) # cam -> world
+        T_CwW = np.linalg.inv(T_WCw) # world -> cam
+
+        T_CCw = getTCCw() # world system to camera system 
+
+        T_CW = np.matmul(T_CCw, T_CwW) # world -> camera
+
+        vertex = []
+        for point in bbox:
+            in_cam_space = np.matmul(T_CW, point) # place the point in camera space
+            in_cam_space_cropped = in_cam_space[:3] # crop to get a 1x3 vector
+            in_px_raw = np.matmul(K, in_cam_space_cropped) # project the point in the camera plane
+            in_px_norm = in_px_raw / in_px_raw[2] # normalize the projection
+            if in_px_raw[2] < 0.0: # if the point is behind the camera (z < 0), return an empty set of points
+                return []
+            vertex.append(in_px_norm) 
+        return vertex
+
+    def compute2Dbbox (self, image, bbox):
+        bbox_vert_as3D = self.compute1bbox(image, bbox) # project the points in the camera plane
+        points = [(int(x[0]), int(x[1])) for x in bbox_vert_as3D] # we need tuple for the points
+
+
+        x_min = +10000
+        x_max = -10000
+        y_min = +10000
+        y_max = -10000
+
+        for pt in points:
+            if pt[0] < x_min:
+                x_min = pt[0]
+            if pt[1] < y_min:
+                y_min = pt[1]
+            if pt[0] > x_max:
+                x_max = pt[0]
+            if pt[1] > y_max:
+                y_max = pt[1]
+        
+        points_out = []
+        points_out.append((x_max, y_max))
+        points_out.append((x_max, y_min))
+        points_out.append((x_min, y_min))
+        points_out.append((x_min, y_max))
+        return points_out
+
+
+    def placeBBoxInWorld(self, bbox):
+
+        # bounding box size
+        l = bbox.size.x
+        w = bbox.size.y
+        h = bbox.size.z
+
+        # Bounding box vertex (following CARLA description)
+        vertex = []
+        vertex.append(np.array([l, -w, -h, 1.0]))
+        vertex.append(np.array([l, w, -h, 1.0]))
+        vertex.append(np.array([-l, w, -h, 1.0]))
+        vertex.append(np.array([-l, -w, -h, 1.0]))
+        vertex.append(np.array([-l, -w, h, 1.0]))
+        vertex.append(np.array([l, -w, h, 1.0]))
+        vertex.append(np.array([l, w, h, 1.0]))
+        vertex.append(np.array([-l, w, h, 1.0]))
+
+        # Get Tmat to move the points around the car model at 0, 0, 0
+        T_VB = np.identity(4)
+        T_VB[0][3] = bbox.center.position.x
+        T_VB[1][3] = bbox.center.position.y
+        T_VB[2][3] = bbox.center.position.z
+
+        # Apply to the vertex the transformation. The center of the Bounding Box and the vehicle location are different.
+        for idx, v in enumerate(vertex):
+            vertex[idx] = np.matmul(T_VB, v)
+        
+        # get Tmat vehicle -> world
+        T_WV = pose2Tmat(bbox.vehicle)
+
+        # Apply to the vertex the transformation. place every vertex in the world
+        for idx, v in enumerate(vertex):
+            vertex[idx] = np.matmul(T_WV, v)
+
+        return vertex
