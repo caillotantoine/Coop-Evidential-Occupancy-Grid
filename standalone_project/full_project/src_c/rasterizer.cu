ac8315ea3d7a4fdd9956a3fd2ce498566d3a110f
Antoine CAILLOT <caillot@esigelec.fr>
2021-12-03 17:54:44 +0100

    drawing

--- standalone_project/full_project/src_c/rasterizer.cu
+++ standalone_project/full_project/src_c/rasterizer.cu
@@ -18,19 +18,26 @@ using namespace cv;
 
 int main(int argc, char **argv)
 {
-    float fp_vec[1][4][2] = {{{7.73851759, -49.70380452}, {4.39891667, -49.70377771}, {3.45951359, -38.3454241}, {6.0859267,  -38.34544071}}};
-    int label[1] = {1};
+    float fp_vec[2][4][2] = {{{7.73851759, -49.70380452}, {4.39891667, -49.70377771}, {3.45951359, -38.3454241}, {6.0859267,  -38.34544071}},
+                             {{2.70641359, -48.17095769}, {0.8450105,  -48.17094317}, {0.72571429, -40.87830044}, {2.3243211,  -40.87831116}}};
 
-    Mat map = Mat::zeros(GRIDSIZE, GRIDSIZE, CV_8U);
+
+    int label[2] = {1, 2};
+
+    Mat map = Mat::zeros(GRIDSIZE, GRIDSIZE, CV_8UC4);
 
     int i, j;
     for (i = 0; i<4; i++)
     {
         printf("<%3.2f; %3.2f>\n", fp_vec[0][i][0], fp_vec[0][i][1]);
     }
+    for (i = 0; i<4; i++)
+    {
+        printf("<%3.2f; %3.2f>\n", fp_vec[1][i][0], fp_vec[1][i][1]);
+    }
     printf("test nvcc!!!!!\n");
-    ellipse(map, Point(GRIDSIZE/4, GRIDSIZE/4), Size(GRIDSIZE/4, GRIDSIZE/4), 0, 0, 360, 0xff, 2, 8);
-    projector_cpp(1, label, (float*) fp_vec, map.ptr());
+    // ellipse(map, Point(GRIDSIZE/4, GRIDSIZE/4), Size(GRIDSIZE/4, GRIDSIZE/4), 0, 0, 360, 0xff, 2, 8);
+    projector_cpp(2, label, (float*) fp_vec, map.ptr());
 
     imshow("Test map display", map);
     waitKey(0);
@@ -45,13 +52,14 @@ void bonjour_cpp()
 void projector_cpp(int len, int* labels, float* fp_vec, unsigned char* map)
 {
     int i, j;
-    Mat n_map = Mat(GRIDSIZE, GRIDSIZE, CV_8U, map);
+    Mat n_map = Mat(GRIDSIZE, GRIDSIZE, CV_8UC4, map);
     Point fp[1][4];
     float fx, fy;
     int x, y;
-    float* addr_x, addr_y;
+    float *addr_x, *addr_y;
+    float stepgrid = ((float) MAPSIZE) / GRIDSIZE;
 
-    ellipse(n_map, Point(GRIDSIZE/2, GRIDSIZE/2), Size(GRIDSIZE/4, GRIDSIZE/4), 0, 0, 360, 0xff, 2, 8);
+    // ellipse(n_map, Point(GRIDSIZE/2, GRIDSIZE/2), Size(GRIDSIZE/4, GRIDSIZE/4), 0, 0, 360, 0xff, 2, 8);
     
     // printf("%3.2f\n", *(fp_vec + 3));
     for(i=0; i<len; i++)
@@ -63,13 +71,35 @@ void projector_cpp(int len, int* labels, float* fp_vec, unsigned char* map)
             addr_y = (fp_vec + 8*i + 2*j + 1);
             fx = *addr_x;
             fy = *addr_y;
-            x = 0;
-            y = 0;
+            x = ((int) (fx / stepgrid)) + (GRIDSIZE / 2);
+            y = ((int) (fy / stepgrid)) + (GRIDSIZE / 2);
             printf("<%3.2f; %3.2f> \t <%d; %d> \t(%d, %d) \n", fx, fy, x, y, addr_x, addr_y);
-            
+            fp[0][j] = Point(x, y);
         }
+        const Point* pts[1] = {fp[0]};
+        int npt[] = {4};
+        Scalar labelc;
+        switch(labels[i])
+        {
+            case 1: // Vehicle
+                labelc = Scalar(0xff, 0, 0, 0xff);
+                break;
+
+            case 2: // Pedestrian
+                labelc = Scalar(0, 0, 0xff, 0xff);
+                break;
+
+            case 3: // Terrain
+                labelc = Scalar(0, 0xff, 0, 0xff);
+                break;
+
+            default:
+                labelc = Scalar(0, 0, 0, 0xff);
+                break;
+        }
+        fillPoly( n_map, pts, npt, 1, labelc, LINE_8);
     }
 
-    memcpy(map, n_map.ptr(), GRIDSIZE*GRIDSIZE);
-    delete n_map;
+    memcpy(map, n_map.ptr(), GRIDSIZE*GRIDSIZE*4);
+    // delete n_map;
 }
