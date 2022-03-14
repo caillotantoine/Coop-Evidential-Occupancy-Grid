e0fa796a79ce4b0a60a8f8810ab9587636c1e448
Antoine CAILLOT <caillot@esigelec.fr>
2022-03-14 14:17:03 +0100

    Test diffrent configurations

---
+++ run_one_config_file.sh
@@ -0,0 +1,10 @@
+# Config file name after "config_" with no extention 
+configname="perfect_full_testBBA13"
+
+# Run for each algorithm
+echo "Dempster"
+python3 ./standalone_project/full_project/testbench.py --algo Dempster --save_img True --mean True --json_path ./standalone_project/full_project/configs/config_$configname.json --save_path ~/Desktop/Output_Algo/short/$configname --start 100 --end 200
+echo "Conjunctive"
+python3 ./standalone_project/full_project/testbench.py --algo Conjunctive --save_img True --mean False --json_path ./standalone_project/full_project/configs/config_$configname.json --save_path ~/Desktop/Output_Algo/short/$configname --start 100 --end 200
+echo "Disjonctive"
+python3 ./standalone_project/full_project/testbench.py --algo Disjunctive --save_img True --mean False --json_path ./standalone_project/full_project/configs/config_$configname.json --save_path ~/Desktop/Output_Algo/short/$configname --start 100 --end 200
